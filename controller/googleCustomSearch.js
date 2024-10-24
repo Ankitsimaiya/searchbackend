61f6a3fd0801b9cf1c7aa35de80e7966af8192d0
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2024-10-24 10:21:26 +0530

    remove unnecssary console

--- controller/googleCustomSearch.js
+++ controller/googleCustomSearch.js
@@ -6,7 +6,7 @@ const googleCustomSearch = async (req,res) => {
     const url = `https://www.googleapis.com/customsearch/v1?q=${searchTerm}&key=${process.env.GOOGLE_API_KEY || "AIzaSyBKyB-AvtSNNMNeud8-TjnZ-2afkvC-fZQ"}&cx=${process.env.GOOGLE_CX || "a717e4bcf750c4ab6" }`;
 console.log(url)
     try {
-        console.log("response")
+        // console.log("response")
         const response = await axios.get(url);
         console.log("response")
         res.json(response.data.items);
