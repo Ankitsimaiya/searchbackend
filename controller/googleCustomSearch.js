fa1066b3460522f5a9ab39c518472672f1421b00
Ankitsimaiya <91364583+Ankitsimaiya@users.noreply.github.com>
2024-10-24 23:23:25 +0530

    env setup

--- controller/googleCustomSearch.js
+++ controller/googleCustomSearch.js
@@ -3,11 +3,11 @@ const axios = require('axios')
 const googleCustomSearch = async (req,res) => {
     const searchTerm = req.query.q
 
-    const url = `https://www.googleapis.com/customsearch/v1?q=${searchTerm}&key=${process.env.GOOGLE_API_KEY || "AIzaSyBKyB-AvtSNNMNeud8-TjnZ-2afkvC-fZQ"}&cx=${process.env.GOOGLE_CX || "a717e4bcf750c4ab6" }`;
-console.log(url) 
+    const url = `https://www.googleapis.com/customsearch/v1?q=${searchTerm}&key=${process.env.GOOGLE_API_KEY }&cx=${process.env.GOOGLE_CX  }`;
     try {
         
         const response = await axios.get(url);
+        console.log(response.data.items) 
        
         res.json(response.data.items);
     } catch (error) {
--- controller/youtube.js
+++ controller/youtube.js
@@ -1,20 +1,18 @@
-const { default: axios } = require("axios")
+const { default: axios } = require("axios");
 
-const youtube = async (req,res) => {
-    const YOUTUBE_API_KEY = "AIzaSyCodcvFJHBsxLWFTzGLFEHeFyCnyztWRFk"
-    const searchTerm = req.query.q
-    console.log("searchTerm" , searchTerm)
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&key=${YOUTUBE_API_KEY}&type=video`
-    console.log(url)
-    try {
-        const response = await axios.get(url)
-        console.log("data 1 " ,response.data)
-        console.log("res",response.data.items)
-       return res.status(200).json(response.data.items)
-    } catch (error) { 
-        res.status(500).json({msg : "error",error})
-    } 
-    // res.json({msg:"jbkdg"})  
-} 
+const youtube = async (req, res) => {
+  const searchTerm = req.query.q;
+  console.log("searchTerm", searchTerm);
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&key=${process.env.YOUTUBE_API_KEY}&type=video`;
+  console.log(url);
+  try {
+    const response = await axios.get(url);
 
-module.exports = youtube
+    return res.status(200).json(response.data.items);
+  } catch (error) {
+    res.status(500).json({ msg: "error", error });
+  }
+  // res.json({msg:"jbkdg"})
+};
+
+module.exports = youtube;
